a3cb4a64626c1f67eba41f4cb8e9bfd7a6ebf64a
Jeren Mckey <jerenmckey@gmail.com>
2018-10-07 14:09:42 -0700

    Change to Main Scene

---
+++ Missile Mania/Missile Mania/Assets/Scripts/LauncherVector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherVector : MonoBehaviour {
+
+	void OnEnable()
+    {
+        RotateEventManager.onTouch += RotatePivot;
+    }
+
+    void OnDisable()
+    {
+        RotateEventManager.onTouch -= RotatePivot;
+    }
+
+    public void RotatePivot(Vector2 myTouch)
+    {
+        gameObject.transform.rotation = Quaternion.Euler(0f, myTouch.x * 1.0f, 0f);
+    }
+}
---
+++ Missile Mania/Missile Mania/Assets/Scripts/RotateEventManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateEventManager : MonoBehaviour {
+
+    public delegate void TouchAction(Vector2 myTouch);
+    public static event TouchAction onTouch;
+
+    public void OnTouch()
+    {
+        if (onTouch != null)
+        {
+            Vector2 myTouch = Input.GetTouch(0).deltaPosition;
+            onTouch(myTouch);
+        }
+    }
+}
